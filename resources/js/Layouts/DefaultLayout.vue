7f87fc58403096b858d415768657c8d7d585ec80
Shauli <sluahs@gmail.com>
2023-10-30 12:12:07 +0200

    more initial setup

--- resources/js/Layouts/DefaultLayout.vue
+++ resources/js/Layouts/DefaultLayout.vue
@@ -1,118 +1,132 @@
 <template>
-    <div class="flex justify-between mx-5">
-        <span>Default Layout{{ user?.name }}</span>
-        <Menu as="div" class="relative inline-block">
-            <MenuButton
-                class="inline-flex justify-center rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
-            >
-                <ChevronDownIcon
-                    class="ml-2 -mr-1 h-5 w-5 text-white ui-open:rotate-180 transition-all"
-                    aria-hidden="true"
-                />
-            </MenuButton>
-
-            <transition
-                enter-active-class="transition duration-100 ease-out"
-                enter-from-class="transform scale-95 opacity-0"
-                enter-to-class="transform scale-100 opacity-100"
-                leave-active-class="transition duration-75 ease-in"
-                leave-from-class="transform scale-100 opacity-100"
-                leave-to-class="transform scale-95 opacity-0"
-            >
-                <MenuItems
-                    class="absolute right-0 z-10 mt-2 w-48 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none"
+    <div class="flex flex-col min-h-screen">
+        <div class="header flex justify-between mx-5">
+            <div class="shrink-0 flex items-center">
+                <Link :href="route('home')">
+                    <ApplicationLogo
+                        class="block h-9 w-auto fill-current text-gray-800"
+                    />
+                </Link>
+            </div>
+            <span>Default Layout{{ user?.name }}</span>
+            <!-- Logo -->
+            <Menu as="div" class="relative inline-block">
+                <MenuButton
+                    class="inline-flex justify-center rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
                 >
-                    <div class="px-1 py-1">
-                        <MenuItem v-slot="{ active }">
-                            <button
-                                :class="[
-                                    active
-                                        ? 'bg-violet-500 text-white'
-                                        : 'text-gray-900',
-                                    'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                                ]"
-                            >
-                                Edit
-                            </button>
-                        </MenuItem>
-                        <MenuItem v-slot="{ active }" v-if="user">
-                            <button
-                                :class="[
-                                    active
-                                        ? 'bg-violet-500 text-white'
-                                        : 'text-gray-900',
-                                    'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                                ]"
-                                @click="router.post(route('logout'))"
-                            >
-                                Log Out
-                            </button>
-                        </MenuItem>
-                        <MenuItem v-slot="{ active }" v-else>
-                            <button
-                                :class="[
-                                    active
-                                        ? 'bg-violet-500 text-white'
-                                        : 'text-gray-900',
-                                    'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                                ]"
-                                @click="router.get(route('login'))"
-                            >
-                                Log In
-                            </button>
-                        </MenuItem>
-                    </div>
-                    <div class="px-1 py-1">
-                        <MenuItem v-slot="{ active }">
-                            <button
-                                :class="[
-                                    active
-                                        ? 'bg-violet-500 text-white'
-                                        : 'text-gray-900',
-                                    'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                                ]"
-                            >
-                                Archive
-                            </button>
-                        </MenuItem>
-                        <MenuItem v-slot="{ active }">
-                            <button
-                                :class="[
-                                    active
-                                        ? 'bg-violet-500 text-white'
-                                        : 'text-gray-900',
-                                    'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                                ]"
-                            >
-                                Move
-                            </button>
-                        </MenuItem>
-                    </div>
-
-                    <div class="px-1 py-1">
-                        <MenuItem v-slot="{ active }">
-                            <button
-                                :class="[
-                                    active
-                                        ? 'bg-violet-500 text-white'
-                                        : 'text-gray-900',
-                                    'group flex w-full items-center rounded-md px-2 py-2 text-sm',
-                                ]"
-                            >
-                                Delete
-                            </button>
-                        </MenuItem>
-                    </div>
-                </MenuItems>
-            </transition>
-        </Menu>
+                    <svg
+                        class="ml-2 -mr-0.5 h-4 w-4 ui-open:rotate-180 transition-all"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                    >
+                        <path
+                            fill-rule="evenodd"
+                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                            clip-rule="evenodd"
+                        />
+                    </svg>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-6 h-6"
+                    >
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                        />
+                    </svg>
+                </MenuButton>
+                <transition
+                    enter-active-class="transition duration-100 ease-out"
+                    enter-from-class="transform scale-95 opacity-0"
+                    enter-to-class="transform scale-100 opacity-100"
+                    leave-active-class="transition duration-75 ease-in"
+                    leave-from-class="transform scale-100 opacity-100"
+                    leave-to-class="transform scale-95 opacity-0"
+                >
+                    <MenuItems
+                        class="absolute right-0 z-10 mt-2 w-48 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none"
+                    >
+                        <div class="px-1 py-1">
+                            <MenuItem v-slot="{ active }" v-if="user">
+                                <button
+                                    @click="router.get(route('profile.edit'))"
+                                    :class="[
+                                        active
+                                            ? 'bg-violet-500 text-white'
+                                            : 'text-gray-900',
+                                        'group flex w-full items-center rounded-md px-2 py-2 text-sm',
+                                    ]"
+                                >
+                                    Profile
+                                </button>
+                            </MenuItem>
+                            <MenuItem v-slot="{ active }">
+                                <button
+                                    :class="[
+                                        active
+                                            ? 'bg-violet-500 text-white'
+                                            : 'text-gray-900',
+                                        'group flex w-full items-center rounded-md px-2 py-2 text-sm',
+                                    ]"
+                                    @click="
+                                        user
+                                            ? router.post(route('logout'))
+                                            : router.get(route('login'))
+                                    "
+                                >
+                                    {{ user ? "Log Out" : "Log In" }}
+                                </button>
+                            </MenuItem>
+                        </div>
+                        <div class="px-1 py-1">
+                            <MenuItem v-slot="{ active }" v-if="!user">
+                                <button
+                                    @click="router.get(route('register'))"
+                                    :class="[
+                                        active
+                                            ? 'bg-violet-500 text-white'
+                                            : 'text-gray-900',
+                                        'group flex w-full items-center rounded-md px-2 py-2 text-sm',
+                                    ]"
+                                >
+                                    Register
+                                </button>
+                            </MenuItem>
+                        </div>
+                        <div class="px-1 py-1">
+                            <MenuItem v-slot="{ active }">
+                                <button
+                                    :class="[
+                                        active
+                                            ? 'bg-violet-500 text-white'
+                                            : 'text-gray-900',
+                                        'group flex w-full items-center rounded-md px-2 py-2 text-sm',
+                                    ]"
+                                >
+                                    Delete
+                                </button>
+                            </MenuItem>
+                        </div>
+                    </MenuItems>
+                </transition>
+            </Menu>
+        </div>
+        <div class="grow grid">
+            <slot />
+        </div>
+        <footer class="w-full">footer</footer>
     </div>
-    <slot />
-    <footer class="w-full">footer</footer>
 </template>
 <script setup lang="ts">
 import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
 import { ChevronDownIcon } from "@heroicons/vue/20/solid";
 import { user } from "../Composables/isAuthenticated";
-import { router } from "@inertiajs/vue3";
+import { Link, router } from "@inertiajs/vue3";
+import ApplicationLogo from "@/Components/ApplicationLogo.vue";
 </script>
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('Home');
-});
+})->name('home');
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
